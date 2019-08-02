7ed6b396c84c8f08028d7bbe60cd6f9b49d49b01
0xE4s0n <1181499949@qq.com>
2019-08-02 11:56:37 +0800

    不使用service的下载实例

--- app/src/main/java/e4s0n/servicebestpractice/MainActivity.java
+++ app/src/main/java/e4s0n/servicebestpractice/MainActivity.java
@@ -2,12 +2,18 @@ package e4s0n.servicebestpractice;
 
 
 import android.Manifest;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,18 +21,48 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private DownloadService.DownloadBinder downloadBinder;
-    private ServiceConnection connection = new ServiceConnection() {
+    private DownloadTask downloadTask;
+    private String downloadUrl;
 
+    private DownloadListener listener = new DownloadListener() {
         @Override
-        public void onServiceDisconnected(ComponentName name) {
+        public void onProgress(int progress) {
+            getNotificationManager().notify(1, getNotification("Downloading...", progress));
         }
 
         @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            downloadBinder = (DownloadService.DownloadBinder) service;
+        public void onSuccess() {
+            downloadTask = null;
+            // 下载成功时将前台服务通知关闭，并创建一个下载成功的通知
+            getNotificationManager().cancel(1);
+            getNotificationManager().notify(1, getNotification("Download Success", -1));
+            Toast.makeText(MainActivity.this, "Download Success", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onFailed() {
+            downloadTask = null;
+            // 下载失败时将前台服务通知关闭，并创建一个下载失败的通知
+            getNotificationManager().cancel(1);
+            getNotificationManager().notify(1, getNotification("Download Failed", -1));
+            Toast.makeText(MainActivity.this, "Download Failed", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onPaused() {
+            downloadTask = null;
+            Toast.makeText(MainActivity.this, "Paused", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onCanceled() {
+            downloadTask = null;
+            getNotificationManager().cancel(1);
+            Toast.makeText(MainActivity.this, "Canceled", Toast.LENGTH_SHORT).show();
         }
 
     };
@@ -41,9 +77,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startDownload.setOnClickListener(this);
         pauseDownload.setOnClickListener(this);
         cancelDownload.setOnClickListener(this);
-        Intent intent = new Intent(this, DownloadService.class);
-        startService(intent); // 启动服务
-        bindService(intent, connection, BIND_AUTO_CREATE); // 绑定服务
+//        Intent intent = new Intent(this, DownloadService.class);
+//        startService(intent); // 启动服务
+//        bindService(intent, connection, BIND_AUTO_CREATE); // 绑定服务
         if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission. WRITE_EXTERNAL_STORAGE }, 1);
         }
@@ -51,19 +87,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        if (downloadBinder == null) {
-            return;
-        }
         switch (v.getId()) {
             case R.id.start_download:
                 String url = "https://ctf.bugku.com/files/46839844a2b6a297615d94a3598df811/baby.zip";
-                downloadBinder.startDownload(url);
+                startDownload(url);
                 break;
             case R.id.pause_download:
-                downloadBinder.pauseDownload();
+                pauseDownload();
                 break;
             case R.id.cancel_download:
-                downloadBinder.cancelDownload();
+                cancelDownload();
                 break;
             default:
                 break;
@@ -83,10 +116,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unbindService(connection);
+    public void startDownload(String url) {
+        if (downloadTask == null) {
+            downloadUrl = url;
+            downloadTask = new DownloadTask(listener);
+            downloadTask.execute(downloadUrl);
+            getNotificationManager().notify(1, getNotification("Downloading...", 0));
+            Toast.makeText(MainActivity.this, "Downloading...", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void pauseDownload() {
+        if (downloadTask != null) {
+            downloadTask.pauseDownload();
+        }
+    }
+
+    public void cancelDownload() {
+        if (downloadTask != null) {
+            downloadTask.cancelDownload();
+        } else {
+            if (downloadUrl != null) {
+                // 取消下载时需将文件删除，并将通知关闭
+                String fileName = downloadUrl.substring(downloadUrl.lastIndexOf("/"));
+                String directory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath();
+                File file = new File(directory + fileName);
+                if (file.exists()) {
+                    file.delete();
+                }
+                getNotificationManager().cancel(1);
+                getNotificationManager().cancel(1);
+                Toast.makeText(MainActivity.this, "Canceled", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private NotificationManager getNotificationManager() {
+        return (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    private Notification getNotification(String title, int progress) {
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentIntent(pi);
+        builder.setContentTitle(title);
+        if (progress >= 0) {
+            // 当progress大于或等于0时才需显示下载进度
+            builder.setContentText(progress + "%");
+            builder.setProgress(100, progress, false);
+        }
+        return builder.build();
     }
 
 }
